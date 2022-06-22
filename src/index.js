35b71baa47a70bfef053c77bfd4ccfab71c71916
Manuel Raygoza <Manny.raygoza@gmail.com>
2022-06-21 17:40:27 -0700

    WIP: Needs Lint.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Triangle from './triangle.js';
+import './css/styles.css';
 
-
-const Triangle = require('./triangle.js').triangleModule;
+//const Triangle = require('./triangle.js').triangleModule;
 const triangle = new Triangle(3,3,3);
 
